1aef8451ad5ebda195b8733faaeee1c15672b958
scottj <scottj@SCOTTJ.ad.itgfirm.com>
2018-07-27 21:04:38 -0400

    Disabled security

--- src/main/java/com/github/justinericscott/docengine/controller/SecurityRestController.java
+++ src/main/java/com/github/justinericscott/docengine/controller/SecurityRestController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Justin Scott
