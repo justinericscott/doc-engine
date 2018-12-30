9f22c3c5a2049cc0799dec3cbdb27a151082cd7e
scottj <scottj@SCOTTJ.ad.itgfirm.com>
2018-12-30 00:19:40 -0500

    Enabled SSL

--- src/main/java/com/github/justinericscott/docengine/config/SecurityConfig.java
+++ src/main/java/com/github/justinericscott/docengine/config/SecurityConfig.java
@@ -1,66 +1,56 @@
 package com.github.justinericscott.docengine.config;
-//package com.itgfirm.docengine.security;
-//
+
 //import java.io.IOException;
-//
+
 //import javax.servlet.Filter;
 //import javax.servlet.FilterChain;
 //import javax.servlet.ServletException;
 //import javax.servlet.http.Cookie;
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
-//
-//import org.apache.log4j.LogManager;
-//import org.apache.log4j.Logger;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 //import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.security.SecurityProperties;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.web.csrf.CsrfFilter;
 //import org.springframework.security.web.csrf.CsrfToken;
 //import org.springframework.security.web.csrf.CsrfTokenRepository;
 //import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 //import org.springframework.web.filter.OncePerRequestFilter;
 //import org.springframework.web.util.WebUtils;
-//
-///**
-// * @author Justin Scott TODO: Description
-// */
-//@Configuration
-//@EnableWebMvcSecurity
-//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//	@SuppressWarnings("unused")
+
+/**
+ * @author Justin Scott TODO: Description
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	private static final Logger LOG = LogManager.getLogger(SecurityConfig.class);
 //	private static final String COOKIE = "XSRF-TOKEN";
 //	private static final String CSRF_HEADER = "X-XSRF-TOKEN";
-//	private static final String[] PUBLIC_CONTENT = new String[] {
-//			"/", "/angular/**", "/html/**", "/html/**", "/css/**", "/js/**"
-//	};
-//
-//	public SecurityConfig() {}
-//
+//	private static final String[] PUBLIC_CONTENT = new String[] { "/", "/angular/**", "/html/**", "/html/**", "/css/**",
+//			"/js/**" };
+
+	public SecurityConfig() {
+	}
+
 //	@Autowired
-//	public void registerGlobalAuthentication(AuthenticationManagerBuilder auth)
-//			throws Exception {
+//	public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 //		// TODO: Trace log entry
 //		auth.inMemoryAuthentication().withUser("user").password("pass").roles("user");
 //	}
-//
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		// TODO: Trace log entry
-//		http.formLogin().and().logout().and().authorizeRequests().antMatchers(PUBLIC_CONTENT)
-//				.permitAll().anyRequest().authenticated().and().csrf()
-//				.csrfTokenRepository(csrfTokenRepository()).and()
-//				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
-//	}
-//
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO: Trace log entry
+		http.formLogin().disable().logout().disable();
+	}
+
 //	private Filter csrfHeaderFilter() {
 //		// TODO: Trace log entry
 //		return new OncePerRequestFilter() {
@@ -82,11 +72,11 @@ package com.github.justinericscott.docengine.config;
 //			}
 //		};
 //	}
-//
+
 //	private CsrfTokenRepository csrfTokenRepository() {
 //		// TODO: Trace log entry
 //		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
 //		repository.setHeaderName(CSRF_HEADER);
 //		return repository;
 //	}
-//}
+}
